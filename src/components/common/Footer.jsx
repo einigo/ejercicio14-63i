db792602cb10edefe692e7d8093ddff5a88c1c75
Enzo Iñigo <enzoinigo96@gmail.com>
2023-11-22 23:15:21 -0300

    estilizo navbar y footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,6 +1,11 @@
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className="bg-dark">
+      <section className="container text-light py-5">
+        <p>&copy; Todos los derechos reservados</p>
+        <h5>La cocinita 🧑‍🍳🧑</h5>
+      </section>
+    </footer>
   )
 }
 
